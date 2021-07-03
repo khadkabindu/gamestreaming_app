9c11d2b4e8b8d630fd3f4c44241cff78c52aef24
khadkabindu <bindu.khadka.np@gmail.com>
2021-07-03 20:23:00 +0545

    add details

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -14,6 +14,24 @@ class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin {
     super.initState();
   }
 
+  List<String> images = [
+    "images/Call of Duty_ Mobile.jpg",
+    "images/unnamedleague.jpg",
+    "images/apex-legends.jpg",
+    "images/fortnite---button-1520296499714.jpg"
+  ];
+
+  List<String> names = [
+    "Call of Duty",
+    "League of\nLegends",
+    "Apex Legends",
+    "Fortnite"
+  ];
+
+  List<String> views= ["256.21K", "41.3K", "10.1K", "12.32K"];
+
+  List<String> followers = ["10.2 M", "4 M","92 M","22.1 M"];
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -72,6 +90,7 @@ class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin {
               width: 250,
               child: TabBar(
                 controller: tabController,
+
                 tabs: [
                   Text("Categories"),
                   Text("Live Channels"),
@@ -79,95 +98,119 @@ class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin {
               ),
             ),
             SizedBox(
-              height: 20,
+              height: 25,
             ),
-            Row(
-              children: [
-                Container(
-                  height: 170,
-                  width: 120,
-                  decoration: BoxDecoration(
-                    image: DecorationImage(
-                      image: AssetImage("images/unnamedleague.jpg"),
-                      fit: BoxFit.cover,
-                    ),
-                  ),
-                  // color: Colors.blue,
-                ),
-                SizedBox(
-                  width: 25,
-                ),
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      "League of\nLegends",
-                      style: TextStyle(
-                          color: Colors.white, fontSize: 20, letterSpacing: 1),
-                    ),
-                    SizedBox(
-                      height: 15,
-                    ),
-                    Row(
+            Flexible(
+              child: ListView.builder(
+                  itemCount: 4,
+                  shrinkWrap: true,
+                  scrollDirection: Axis.vertical,
+                  itemBuilder: (context, index) {
+                    return Column(
                       children: [
-                        Icon(
-                          Icons.remove_red_eye_sharp,
-                          color: Colors.white,
-                          size: 20,
-                        ),
-                        SizedBox(
-                          width: 10,
-                        ),
-                        Text(
-                          "256.23K",
-                          style: TextStyle(color: Colors.white),
-                        ),
-                        SizedBox(
-                          width: 45,
-                        ),
-                        Icon(Icons.people, color: Colors.white, size: 20,),
-                        Text(
-                          "  22.3 M",
-                          style: TextStyle(color: Colors.white),
-                        )
-                      ],
-                    ),
-                    SizedBox(
-                      height: 20,
-                    ),
-                    Row(
-                      children: [
-                        Container(
-                          height: 25,
-                          width: 74,
-                          child: RaisedButton(
-                            onPressed: () {},
-                            color: Color(0xff2a2626),
-                            child: Text(
-                              "Export",
-                              style: TextStyle(color: Colors.grey),
+                        Row(
+                          children: [
+                            Container(
+                              height: 170,
+                              width: 120,
+                              decoration: BoxDecoration(
+                                image: DecorationImage(
+                                  image: AssetImage(images[index]),
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                              // color: Colors.blue,
                             ),
-                          ),
+                            SizedBox(
+                              width: 25,
+                            ),
+                            Column(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  names[index],
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 20,
+                                      letterSpacing: 1),
+                                ),
+                                SizedBox(
+                                  height: 15,
+                                ),
+                                Row(
+                                  children: [
+                                    Icon(
+                                      Icons.remove_red_eye_sharp,
+                                      color: Colors.white,
+                                      size: 20,
+                                    ),
+                                    SizedBox(
+                                      width: 10,
+                                    ),
+                                    Text(
+                                      views[index],
+                                      style: TextStyle(color: Colors.white),
+                                    ),
+                                    SizedBox(
+                                      width: 45,
+                                    ),
+                                    Icon(
+                                      Icons.people,
+                                      color: Colors.white,
+                                      size: 20,
+                                    ),
+                                    Text(
+                                      "  ${followers[index]}",
+                                      style: TextStyle(color: Colors.white),
+                                    )
+                                  ],
+                                ),
+                                SizedBox(
+                                  height: 20,
+                                ),
+                                Row(
+                                  children: [
+                                    Container(
+                                      height: 25,
+                                      width: 74,
+                                      child: RaisedButton(
+                                        onPressed: () {},
+                                        color: Color(0xff2a2626),
+                                        child: Text(
+                                          "Export",
+                                          style: TextStyle(color: Colors.grey),
+                                        ),
+                                      ),
+                                    ),
+                                    SizedBox(
+                                      width: 10,
+                                    ),
+                                    Container(
+                                      height: 25,
+                                      width: 74,
+                                      child: RaisedButton(
+                                        onPressed: () {},
+                                        color: Color(0xff2a2626),
+                                        child: Text(
+                                          "MOBA",
+                                          style: TextStyle(color: Colors.grey),
+                                        ),
+                                      ),
+                                    )
+                                  ],
+                                )
+                              ],
+                            )
+                          ],
                         ),
                         SizedBox(
-                          width: 10,
-                        ),
-                        Container(
-                          height: 25,
-                          width: 74,
-                          child: RaisedButton(
-                            onPressed: () {},
-                            color: Color(0xff2a2626),
-                            child: Text("MOBA", style: TextStyle(color: Colors.grey),),
-                          ),
+                          height: 20,
                         )
                       ],
-                    )
-                  ],
-                )
-              ],
-            ),
+                    );
+                  }),
+            )
           ],
         ),
       ),
