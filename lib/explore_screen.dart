04faacb2cc790d9adc744657d4e9a4a3f9f0ec6a
khadkabindu <bindu.khadka.np@gmail.com>
2021-07-04 20:26:29 +0545

    add image caraousel to explore screen

--- lib/explore_screen.dart
+++ lib/explore_screen.dart
@@ -1,6 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:carousel_pro/carousel_pro.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 
 class ExploreScreen extends StatelessWidget {
+  List items = [
+    "images/dwgvst1.jpg",
+    "images/MW_S4_TwitchDrop_Tout.jpg",
+    "images/AGB_Twitch_Drop_2.jpg",
+    "images/phreakandkode.jpg"
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -8,6 +16,7 @@ class ExploreScreen extends StatelessWidget {
       child: Padding(
         padding: EdgeInsets.all(15),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Row(
               mainAxisAlignment: MainAxisAlignment.end,
@@ -39,6 +48,45 @@ class ExploreScreen extends StatelessWidget {
                 ),
               ],
             ),
+            SizedBox(
+              height: 5,
+            ),
+            Text(
+              "Discover",
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            CarouselSlider(
+                items: items
+                    .map((e) => Column(
+                          children: [
+                            Expanded(
+                              child: Container(
+                                height: 300,
+                                width: 265,
+                                decoration: BoxDecoration(
+                                    image: DecorationImage(
+                                        image: AssetImage(e),
+                                        fit: BoxFit.cover)),
+                              ),
+                            ),
+                            CircleAvatar(
+                              backgroundImage: AssetImage(
+                                  "images/istockphoto-1218726670-612x612.jpg"),
+                            )
+                          ],
+                        ))
+                    .toList(),
+                options: CarouselOptions(
+                  autoPlayInterval: Duration(seconds: 2),
+                  autoPlayAnimationDuration: Duration(milliseconds: 800),
+                  scrollDirection: Axis.horizontal,
+                ),)
           ],
         ),
       ),
